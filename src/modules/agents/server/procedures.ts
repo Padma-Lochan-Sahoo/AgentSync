fa851c833427c08b1660d14f9955d6b0938f5759
Padma-Lochan-Sahoo <padmalochansahoo8503@gmail.com>
2025-08-24 15:43:41 +0530

    14: Agent Page

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -6,12 +6,13 @@ import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agentInsertSchema } from "../schemas";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
+import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
   // TODO: change 'getone' to use  'any'
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
-    .query(async ({ input }) => {
+    .query(async ({ input,ctx }) => {
       const [existingAgent] = await db
         .select({
             // TODO: Change to actual counts
@@ -19,8 +20,18 @@ export const agentsRouter = createTRPCRouter({
           ...getTableColumns(agents),
         })
         .from(agents)
-        .where(eq(agents.id, input.id));
-
+        .where(
+          and(
+            eq(agents.id, input.id),
+            eq(agents.userId, ctx.auth.user.id),
+          )
+        );
+        if(!existingAgent){
+          throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Agent not found",
+          });
+        }
       return existingAgent;
     }),
   getMany: protectedProcedure
