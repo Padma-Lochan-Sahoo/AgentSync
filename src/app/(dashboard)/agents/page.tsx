857fd8db94a2f36de9da20f2f96ffe373170f328
Padma-Lochan-Sahoo <padmalochansahoo8503@gmail.com>
2025-08-24 14:27:35 +0530

    13: Agents Filter

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -14,9 +14,15 @@ import {
 from "@/modules/agents/ui/views/agents-view";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { AgentListHeader } from "@/modules/agents/ui/components/agents-list-header";
+import type { SearchParams } from "nuqs";
+import { loadSearchParams } from "@/modules/agents/params";
 
+interface Props {
+    searchParams: Promise<SearchParams>
+}
 
-const Page = async () => {
+const Page = async ({searchParams} : Props) => {
+    const filters = await loadSearchParams(searchParams);
     const session = await auth.api.getSession({
         headers: await headers(),
       })
@@ -26,7 +32,9 @@ const Page = async () => {
       }
 
     const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+        ...filters,
+    }));
 
     return (
         <>
